2fdbaac722b6a7e61d6d966484ae702216cc1b68
GigaScratch <jamreya9@student.wintec.ac.nz>
2019-08-15 11:39:59 +1200

    GUI designed

---
+++ AccessTask/AccessTask/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace AccessTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.rtb_result = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Table Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(31, 58);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(185, 31);
+            this.tb_name.TabIndex = 2;
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(270, 58);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(185, 31);
+            this.tb_id.TabIndex = 3;
+            // 
+            // rtb_result
+            // 
+            this.rtb_result.Location = new System.Drawing.Point(31, 121);
+            this.rtb_result.Name = "rtb_result";
+            this.rtb_result.Size = new System.Drawing.Size(889, 417);
+            this.rtb_result.TabIndex = 4;
+            this.rtb_result.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 578);
+            this.Controls.Add(this.rtb_result);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Access Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.RichTextBox rtb_result;
+    }
+}
+
